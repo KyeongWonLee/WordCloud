a84b6785acaf047b88e7c964e5fcb90268e781e2
KyeongWonLee <kw0226@playerauctionskorea.com>
2020-06-22 15:28:35 +0900

    Add Router Module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import {HashRouter as Router, Route} from 'react-router-dom';
 import AppShell from './components/AppShell.js';
+import Home from './components/Home';
+import Texts from './components/Texts';
+import Words from './components/Words';
 
 function App() {
   return (
-    <AppShell/>
+    <Router>
+        <AppShell>
+        <div>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/texts" component={Texts}></Route>
+          <Route exact path="/words" component={Words}></Route>
+        </div>
+      </AppShell>
+    </Router>
   );
 }
 
